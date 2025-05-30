eab2f7cb4ace81c7d07c2e38d790af0ff7dd658d
Bence Csati <bence.csati@axoflow.com>
2025-05-30 16:22:28 +0200

    refactor: use loki instead of bare logs in demos
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- KCD/3-LO-TC/cleanup.sh
+++ KCD/3-LO-TC/cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f ./KCD/3-LO-TC/tc-tenant-a-logging.yaml
-kubectl delete -f ./KCD/3-LO-TC/fluentbitagent-infra.yaml
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+kubectl apply -f "${SCRIPT_DIR}/tc-tenant-a-logging.yaml"
+kubectl delete -f "${SCRIPT_DIR}/fluentbitagent-infra.yaml"
